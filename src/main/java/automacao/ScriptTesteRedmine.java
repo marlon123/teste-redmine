90a8dab0859b8d12f667f34cc4281ca0ecb9b341
Marlon Lopes Pinto <marlonlopes.p@gmail.com>
2019-07-25 00:07:12 -0300

    commit com configuração inicial

---
+++ src/main/java/automacao/ScriptTesteRedmine.java
@@ -0,0 +1,49 @@
+package automacao;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ScriptTesteRedmine {
+
+    @FindBy(xpath = "//div[@id='account']//a[@class='register']")
+    WebElement botaoCadastro;
+
+    @FindBy(id = "user_login")
+    WebElement inputCadastroLogin;
+
+    @FindBy(id = "user_password")
+    WebElement inputCadastroSenha;
+
+    @FindBy(id = "user_password_confirmation")
+    WebElement inputConfirmacaoSenha;
+
+    @FindBy(id = "user_firstname")
+    WebElement inputCadastroNome;
+
+    @FindBy(id = "user_lastname")
+    WebElement inputCadastroSobrenome;
+
+    @FindBy(id = "user_mail")
+    WebElement inputCadastroEmail;
+
+    @FindBy(id = "Enviar")
+    WebElement botaoEnviarCadastro;
+
+    @FindBy(xpath = "//div[@id='top-menu']//a[@class='projects']")
+    WebElement menuProjetos;
+
+    @FindBy(xpath = "//div[@class='contextual']//a[contains(text(), 'Novo projeto')]")
+    WebElement menuNovoProjeto;
+
+    public ScriptTesteRedmine(){
+
+    }
+
+    public static void main(String args[]){
+
+    }
+
+    public void teste(){
+        String paginaInicial = "http://demo.redmine.org/";
+    }
+}
